ef00a628af2ee8702f23ef9a2dfc89184e4397fe
Tornike Dzidziguri <tornikedzidziguri2001@gmail.com>
2024-01-16 16:08:45 +0400

    partOne:done

--- src/Components/Backview.jsx
+++ src/Components/Backview.jsx
@@ -6,9 +6,17 @@ import './Backview.css'
 const Backview = () => {
 
   return (
-    <div className='foto'>
+    <div className='backBox'>
+      <header>
      <img src={WWS} className='WWSclass'/>
      <input className='txtInput' type='text' placeholder='Search Location'/>
+    </header>
+    <div className='celInfo'>
+      <h1 className='celh1 '>16°</h1>
+      <h2 className='celh2 '>London</h2>
+      <h3 className='celh3 '>06:09 - Monday, 9 Sep ‘23</h3>
+      <div className='inPick'></div>
+    </div>
     </div>
   )
 }
